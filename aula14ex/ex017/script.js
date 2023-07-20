69ecbc2bdfd8b5693a382be526559e14ab16d0ce
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-07-20 18:15:45 -0300

    exs 017 e 018

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,18 +1,28 @@
 function contar() {
-    var Inicio = window.document.querySelector('input#ini')
-    var Fim = window.document.querySelector('input#fim')
-    var Passo = window.document.querySelector('input#pas')
-    var Resposta = window.document.querySelector('div#res')
-    Inicio = Number(Inicio.value)
-    Fim = Number(Fim.value)
-    Passo = Number(Passo.value)
-    if (Inicio == 0 || Fim == 0) {
-        Resposta.innerHTML = ('Impossível contar!')
-    } else if (Passo == 0) {
-        alert('Passo inválido!. Considerando passo 1')
+    var inicio = window.document.querySelector('input#txti')
+    var fim = window.document.querySelector('input#txtf')
+    var passo = window.document.querySelector('input#txtp')
+    var res = window.document.querySelector('div#res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+        res.innerHTML = ('Impossível contar!')
     } else {
-        for (var c = Inicio;c <= Fim; c += Passo) {
-            Resposta.innerHTML = (c)
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-    }  
+        if (i < f) {
+            for (var c = i;c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for (var c = i;c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function tabuada() {
+    var num = window.document.querySelector('input#txtn')
+    var tab = window.document.querySelector('select#seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for (var c = 1;c <= 10;c++) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
